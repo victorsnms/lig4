523ce6596e480dc3311bbd34e67df9e77f194184
Victor Martins <victorsantows@gmail.com>
2021-06-10 15:13:21 -0300

    adicionado sound slider, falta implementar a função

--- script.js
+++ script.js
@@ -10,13 +10,21 @@ const vencedor = document.getElementsByClassName('vitoria')[0];
 const btnReset = document.getElementById('reset');
 const btnStart = document.getElementById('start');
 const btnMute = document.getElementById('mute');
+const soundControl = document.getElementById('sound-control');
+const volumeControl = document.getElementById('volume-control');
 const sfx = document.getElementById('sfx');
 const victoryTheme = document.getElementById('victoryTheme');
 const backgroundMusic = document.getElementById('soundtrack');
 btnReset.classList.add('botao')
 sfx.volume = 0.25;
+backgroundMusic.volume = 0.7;
+victoryTheme.volume = 0.7;
 sfx.playbackRate = 1.5;
 backgroundMusic.loop = true;
+
+//fazer isso ficar automático;
+backgroundMusic.volume = volumeControl.value / 100
+victoryTheme.volume = volumeControl.value / 100
 
 let modoMusica = true;
 let turno = 1;
@@ -61,6 +69,7 @@ btnStart.addEventListener('click', () => {
     btnStart.classList.add('hidden');
     main.classList.remove('hidden');
     btnMute.classList.remove('hidden');
+    soundControl.classList.remove('hidden');
     backgroundMusic.play();
 })
 
